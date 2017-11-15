d47a2fa7fec4685d8655bf7559b4029bbff66daf
Hendryk Irmischer <h.irmischer@francotyp.com>
2017-11-15 15:38:44 +0100

    1. WIP: Testing npm install

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const del = require('del');
 
-gulp.task('default', ['clean', 'copyTemplates', 'copySrc', 'copyLib']);
+gulp.task('default', ['clean', 'copyTemplates', 'copyLib', 'copySrc']);
 
-gulp.task('clean', function(){
+gulp.task('clean', function() {
   return del(['dist']);
 });
 
-gulp.task('copyTemplates', function(){
+gulp.task('copyTemplates', function() {
   return gulp.src('src/**/*.mustache').pipe(gulp.dest('dist'));
 });
 
-gulp.task('copySrc', function(){
-  return gulp.src('src/**/*.ts').pipe(ts({project: 'tsconfig.json'})).pipe(gulp.dest('dist'));
+gulp.task('copyLib', function() {
+  return gulp.src('src/lib/generator.js').pipe(gulp.dest('dist/lib'));
 });
 
-gulp.task('copyLib', function(){
-  return gulp.src('src/lib/**/*.js').pipe(gulp.dest('dist'));
+gulp.task('copySrc', function() {
+  return gulp.src('src/**/*.ts').pipe(ts({project: 'tsconfig.json'})).pipe(gulp.dest('dist'));
 });
 
