9df7857acee5461f3b6141c25643bd116eb55510
Hendryk Irmischer <h.irmischer@francotyp.com>
2017-11-15 15:30:37 +0100

    1. WIP: Testing npm install via github url

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,21 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const del = require('del');
 
-gulp.task('default', ['clean', 'copyTemplates','copyLib']);
+gulp.task('default', ['clean', 'copyTemplates', 'copySrc', 'copyLib']);
 
 gulp.task('clean', function(){
   return del(['dist']);
 });
-gulp.task('copyTemplates',['clean'], function(){
+
+gulp.task('copyTemplates', function(){
   return gulp.src('src/**/*.mustache').pipe(gulp.dest('dist'));
 });
 
-gulp.task('copyLib',['clean'], function(){
+gulp.task('copySrc', function(){
   return gulp.src('src/**/*.ts').pipe(ts({project: 'tsconfig.json'})).pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyLib', function(){
+  return gulp.src('src/lib/**/*.js').pipe(gulp.dest('dist'));
 });
 
